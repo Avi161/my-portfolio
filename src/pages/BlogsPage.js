78ac9f6476da7731c76e20b69f1438e6e04e716c
avi <paudela@union.edu>
2025-05-13 14:34:25 -0400

    Fix small bug of picture not loading

--- src/pages/BlogsPage.js
+++ src/pages/BlogsPage.js
@@ -115,7 +115,7 @@ const BlogsPage = ({}) => {
                     
                     </div>
                     <div class="carousel-item">
-                      <img src={`${process.env.PUBLIC_URL}/blog/HsFriendsPDS.jpg`} class="d-block w-100" alt="..."/>
+                      <img src={`${process.env.PUBLIC_URL}/blog/HsFriendsPDS.JPG`} class="d-block w-100" alt="..."/>
                       <div class="carousel-caption d-none d-md-block">
                         <h5>Third slide label</h5>
                         <p>Some representative placeholder content for the third slide.</p>
