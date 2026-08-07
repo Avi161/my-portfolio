d891e37d854bf35a492e69a09bf02bbd4e41b9b7
Avi161 <paudela@union.edu>
2026-08-06 19:26:42 -0700

    footer: add Google Scholar link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,8 @@ const Footer = () => (
       Avigya Paudel &middot;{' '}
       <a href="mailto:paudela@union.edu">paudela@union.edu</a> &middot;{' '}
       <a href="https://github.com/Avi161" target="_blank" rel="noopener noreferrer">GitHub</a> &middot;{' '}
-      <a href="https://www.linkedin.com/in/avigya-paudel-531119306/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+      <a href="https://www.linkedin.com/in/avigya-paudel-531119306/" target="_blank" rel="noopener noreferrer">LinkedIn</a> &middot;{' '}
+      <a href="https://scholar.google.com/citations?user=D0SkxC0AAAAJ&hl=en" target="_blank" rel="noopener noreferrer">Google Scholar</a>
     </p>
   </footer>
 );
